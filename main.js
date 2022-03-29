48299fc6335799c31f6e191680de20637712dc2d
David Chan <davidchan125@hotmail.co.uk>
2022-03-29 01:30:32 +0100

    Add more features: - Faster bouncing upon all-grid reveal - Text reveal upon all-grid reveal - A "Quack again?" button, that is also revealed upon all-grid reveal

--- main.js
+++ main.js
@@ -1,9 +1,61 @@
+const cardGridSquares = document.querySelectorAll(".card-grid-square");
 const cardGridSquareIcons = document.querySelectorAll(".card-grid-square-icon");
+const quackingText = document.querySelector(".quacking-text");
+const btnQuackAgain = document.querySelector(".btn-quack-again");
+
+function reset() {
+    cardGridSquareIcons.forEach(icon => {
+        icon.classList.remove("reveal");
+        icon.classList.remove("bounce");
+        icon.classList.remove("faster");
+    });
+    cardGridSquares.forEach(square => {
+        square.classList.remove("border-dark");
+    });
+    quackingText.classList.remove("reveal");
+    quackingText.classList.remove("bounce");
+    quackingText.classList.remove("faster");
+    
+    btnQuackAgain.classList.remove("reveal");
+    btnQuackAgain.classList.remove("bounce");
+    btnQuackAgain.classList.remove("bounce-small");
+}
+
+function revealAndBounceIcon(icon) {
+    icon.classList.add("reveal");
+    icon.classList.add("bounce");
+}
+
+function intensifyQuacking() {
+    setTimeout(() => {
+        btnQuackAgain.classList.add("reveal");
+        btnQuackAgain.classList.add("bounce");
+        btnQuackAgain.classList.add("bounce-small");
+    }, 800);
+    cardGridSquareIcons.forEach(icon => {
+        icon.classList.add("faster");
+    });
+    cardGridSquares.forEach(square => {
+        square.classList.add("border-dark");
+    });
+    quackingText.classList.add("reveal");
+    quackingText.classList.add("bounce");
+    quackingText.classList.add("faster");
+}
 
 cardGridSquareIcons.forEach(icon => {
     icon.addEventListener("click", event => {
         if (!icon.classList.contains("reveal")) {
-            icon.classList.add("reveal");
+            revealAndBounceIcon(icon);
         }
-    })
+
+        const numIconsRevealed = document.querySelectorAll(".card-grid-square-icon.reveal").length;
+        if (numIconsRevealed === cardGridSquareIcons.length) {
+            intensifyQuacking();
+        }
+    });
+});
+
+btnQuackAgain.addEventListener("click", event => {
+    reset();
 });
